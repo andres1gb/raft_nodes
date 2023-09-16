aaf862f9fe8cf100442bb56983c2913f18cc80c3
Andrés Purriños <andres1gb@gmail.com>
2023-09-16 20:53:37 +0200

    IP and port validation

--- utils/address.go
+++ utils/address.go
@@ -1,15 +1,30 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
 	"net/netip"
 )
 
 type Address struct {
-	Addr netip.Addr
-	Port uint64
+	addr netip.Addr
+	port uint64
+}
+
+func NewAddress(addr string, port uint64) (*Address, error) {
+	address, err := netip.ParseAddr(addr)
+	if err != nil {
+		return nil, err
+	}
+	if port == 0 || port > 65535 {
+		return nil, errors.New("port number should be in the 1:65535 range")
+	}
+	return &Address{
+		addr: address,
+		port: port,
+	}, nil
 }
 
 func (a *Address) String() string {
-	return fmt.Sprintf("%s:%d", a.Addr.String(), a.Port)
+	return fmt.Sprintf("%s:%d", a.addr.String(), a.port)
 }
--- utils/address_test.go
+++ utils/address_test.go
@@ -1,13 +1,12 @@
 package utils
 
 import (
-	"net/netip"
 	"testing"
 )
 
 func TestAddress_String(t *testing.T) {
 	type fields struct {
-		Addr netip.Addr
+		Addr string
 		Port uint64
 	}
 	tests := []struct {
@@ -18,7 +17,7 @@ func TestAddress_String(t *testing.T) {
 		{
 			name: "ipv4",
 			fields: fields{
-				Addr: netip.MustParseAddr("127.0.0.1"),
+				Addr: "127.0.0.1",
 				Port: 8080,
 			},
 			want: "127.0.0.1:8080",
@@ -26,7 +25,7 @@ func TestAddress_String(t *testing.T) {
 		{
 			name: "ipv6",
 			fields: fields{
-				Addr: netip.MustParseAddr("2001:db8:a0b:12f0::1"),
+				Addr: "2001:db8:a0b:12f0::1",
 				Port: 9000,
 			},
 			want: "2001:db8:a0b:12f0::1:9000",
@@ -34,10 +33,7 @@ func TestAddress_String(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			a := &Address{
-				Addr: tt.fields.Addr,
-				Port: tt.fields.Port,
-			}
+			a, _ := NewAddress(tt.fields.Addr, tt.fields.Port)
 			if got := a.String(); got != tt.want {
 				t.Errorf("Address.String() = %v, want %v", got, tt.want)
 			}
